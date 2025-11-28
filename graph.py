cc91eb5ca0dbc0a1a884096d7aa428bb23ebe66c
bogosorter <luiswbarbosa@gmail.com>
2025-11-28 20:24:22 +0000

    implemented graph class

---
+++ graph.py
@@ -0,0 +1,25 @@
+class Graph:
+    def __init__(self):
+        self.nodes = set()
+        # edges matches a node to a list of its outgoing edges
+        self.edges = dict()
+
+    def node(self, node):
+        self.nodes.add(node)
+        self.edges[node] = list()
+    
+    def edge(self, u, v):
+        self.edges[u].append(v)
+    
+    def path(self, u, v):
+        return self.dfs(u, v, set())
+
+    def dfs(self, u, v, visited):
+        if u == v: return True
+
+        visited.add(u)
+        for neighbor in self.edges[u]:
+            if neighbor not in visited:
+                if self.dfs(neighbor, v, visited):
+                    return True
+        return False
