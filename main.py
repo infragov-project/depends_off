63437d8f9471db33a5aa0c06d5439a8ab712dc95
bogosorter <luiswbarbosa@gmail.com>
2025-11-28 19:55:56 +0000

    improved dependency parsing

--- main.py
+++ main.py
@@ -1,37 +1,106 @@
 import hcl2
 import re
+import pprint
 
-with open("test.tf", "r") as f:
-    data = hcl2.load(f, with_meta=True)
+def main():
+    resources, dependencies = Parser.parse('test.tf')
 
-def get_name(resource):
-    return '.'.join(resource.split('.')[0:2])
+    print('resources:')
+    for resource in resources: print(resource)
+    print()
 
-resources = list()
-for resource in data['resource']:
-    rtype, content = next(iter(resource.items()))
-    name, attributes = next(iter(content.items()))
-    print(rtype + "." + name)
+    print('dependencies:')
+    for dependency in dependencies: print(dependency)
 
-for resource in data['resource']:
-    rtype, content = next(iter(resource.items()))
-    name, attributes = next(iter(content.items()))
+class Resource:
+    def __init__(self, type, name, range):
+        self.type = type
+        self.name = name
+        self.range = range
 
-    print(rtype + "." + name)
-    for attribute_name, attribute in attributes.items():
-        if attribute_name.startswith('__'):
-            continue
-        print("\t" + attribute_name + " = " + str(attribute['value']))
+    def __str__(self):
+        return f'{self.type}.{self.name} ({self.range})'
 
-        if type(attribute['value']) == list:
-            for value in attribute['value']:
-                match = re.match(r'^\${(.*)}$', value)
-                if match:
-                    print('\t\tdepends on ' + get_name(match.group(1)))
-        else:
-            match = re.match(r'^\${(.*)}$', attribute['value'])
-            if match:
-                print('\t\tdepends on ' + get_name(match.group(1)))
+class Dependency:
+    def __init__(self, dependee, depended, explicit, range):
+        self.dependee = dependee
+        self.depended = depended
+        self.range = range
+        self.explicit = explicit
+    
+    def __str__(self):
+        return f'{self.depended} -> {self.dependee} ({self.range}{', explicit' if self.explicit else ''})'
 
+class Range:
+    def __init__(self, start_line, start_column, end_line, end_column):
+        self.start_line = start_line
+        self.start_column = start_column
+        self.end_line = end_line
+        self.end_column = end_column
 
-#pprint.pprint(resources)
+    def __str__(self):
+        return f'{self.start_line}:{self.start_column}-{self.end_line}:{self.end_column}'
+
+class Parser:
+    @staticmethod
+    def parse(filename):
+        with open(filename, 'r') as f:
+            content = hcl2.load(f, with_meta=True)
+        
+        resources = list()
+        dependencies = list()
+
+        for data in content['resource']: Parser.resource(data, resources, dependencies)
+
+        return (resources, dependencies)
+    
+    @staticmethod
+    def resource(data, resources, dependencies):
+        rtype, metadata = next(iter(data.items()))
+        name, details = next(iter(metadata.items()))
+
+        resource = Resource(rtype, name, Range(
+            details['__start_line__'],
+            details['__start_column__'],
+            details['__end_line__'],
+            details['__end_column__']
+        ))
+        resources.append(resource)
+
+        for attribute, metadata in details.items():
+            if attribute.startswith('__'):
+                continue
+
+            value = metadata['value']
+            if type(value) == list:
+                for v in value:
+                    dependency = Parser.dependency(v, resource, metadata, attribute == 'depends_on')
+                    if dependency: dependencies.append(dependency)
+            else:
+                dependency = Parser.dependency(value, resource, metadata, attribute == 'depends_on')
+                if dependency: dependencies.append(dependency)
+    
+    @staticmethod
+    def dependency(value, resource, metadata, explicit):
+        match = re.match(r'^\${(.*)}$', value)
+        if not match:
+            return None
+        
+        dependee_name = match.group(1)
+        tmp = dependee_name.split('.')
+        dependee_type = tmp[0]
+        dependee_resource = tmp[1]
+
+        return Dependency(
+            f'{dependee_type}.{dependee_resource}',
+            f'{resource.type}.{resource.name}',
+            explicit,
+            Range(
+                metadata['__start_line__'],
+                metadata['__start_column__'],
+                metadata['__end_line__'],
+                metadata['__end_column__']
+            )
+        )
+    
+main()
