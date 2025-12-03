fd69d4c65541da734e5e902d0128675d12e3b71c
bogosorter <luiswbarbosa@gmail.com>
2025-12-03 18:28:08 +0000

    created dependency analyzer class

---
+++ analyzer.py
@@ -0,0 +1,67 @@
+from resources import Resource, Dependency
+from graph import DAG
+
+class DependencyAnalyzer:
+    def __init__(self, resources: list[Resource], dependencies: list[Dependency]):
+        self.resources = resources
+        self.dependencies = dependencies
+
+        self.graph = DAG()
+
+        self.resources_to_node: dict[str, int] = dict()
+        for i, resource in enumerate(self.resources):
+            self.graph.node(i)
+            self.resources_to_node[resource.id()] = i
+
+        for dependency in self.dependencies:
+            u = self.resources_to_node[dependency.dependee]
+            v = self.resources_to_node[dependency.depended]
+            self.graph.edge(u, v)
+
+    def redundant(self):
+        # Some of the edges will be removed, but we need to keep the original
+        # ordering of the graph
+        self.graph.toposort()
+
+        # Remove the explicit edges and add them one by one such that, when an edge is
+        # added, all the edges that an alternative path might have used have already
+        # been added to the graph.
+
+        explicit_dependencies = [d for d in self.dependencies if d.explicit]
+
+        for dependency in explicit_dependencies:
+            u = self.resources_to_node[dependency.dependee]
+            v = self.resources_to_node[dependency.depended]
+            self.graph.remove_edge(u, v)
+
+        # Check if any of the explicit dependencies is redundant
+        explicit_dependencies.sort(key = self.dependency_to_key)
+        redundant_dependencies: list[Dependency] = list()
+
+        for dependency in self.dependencies:
+            if not dependency.explicit: continue
+
+            u = self.resources_to_node[dependency.dependee]
+            v = self.resources_to_node[dependency.depended]
+
+            if self.graph.path(u, v):
+                redundant_dependencies.append(dependency)
+            
+            self.graph.edge(u, v)
+
+        return redundant_dependencies
+
+    def dependency_to_key(self, dependency: Dependency):
+        # We'll order the edges such that they can be added one by one, and when
+        # one edge is inserted all the edges that an alternative path might have
+        # used have already been added to the graph. An edge X = (a, b) comes
+        # before an endge Y = (c, d) if it might be part of a path from c to d.
+        # As such, a <= c and b >= d in the topological ordering.
+
+        u = self.resources_to_node[dependency.dependee]
+        v = self.resources_to_node[dependency.depended]
+
+        a = self.graph.order(u)
+        b = self.graph.order(v)
+
+        return (a, -b)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from resources import Dependency
 from parser import Parser
-from graph import DAG, CycleError
-
+from graph import CycleError
+from analyzer import DependencyAnalyzer
 
 # Parse the Terraform file
 
@@ -10,77 +9,20 @@ resources, dependencies = Parser.parse('test.tf')
 print('resources:')
 for resource in resources: print(resource)
 print()
-
 print('dependencies:')
 for dependency in dependencies: print(dependency)
 print()
 
+# Search for redundant dependencies
 
-# Build the full dependency graph and get its topological order
+try: analyzer = DependencyAnalyzer(resources, dependencies)
+except CycleError:
+    print('error: the dependency graph contains a cycle')
+    exit(1)
 
-graph = DAG()
-
-resources_to_node: dict[str, int] = dict()
-for i, resource in enumerate(resources):
-    graph.node(i)
-    resources_to_node[resource.id()] = i
-
-for dependency in dependencies:
-    u = resources_to_node[dependency.dependee]
-    v = resources_to_node[dependency.depended]
-
-    try: graph.edge(u, v)
-    except CycleError:
-        print(f'the dependency graph has a cycle: cycle detected on {dependency}')
-        exit(1)
-
-order = graph.toposort()
-
-
-# Remove the explicit edges and add them one by one such that, when an edge is
-# added, all the edges that an alternative path might have used have already
-# been used.
-
-explicit_dependencies = [d for d in dependencies if d.explicit]
-
-for dependency in explicit_dependencies:
-    u = resources_to_node[dependency.dependee]
-    v = resources_to_node[dependency.depended]
-    graph.remove_edge(u, v)
-
-# We'll order the edges such that they can be added one by one, respecting the
-# property above. An edge X = (a, b) comes before an endge Y = (c, d) if it
-# might be part of a path from c to d. As such, a <= c and b >= d in the
-# topological ordering.
-
-def dependency_to_key(dependency: Dependency):
-    u = resources_to_node[dependency.dependee]
-    v = resources_to_node[dependency.depended]
-
-    a = graph.order(u)
-    b = graph.order(v)
-
-    return (a, -b)
-
-explicit_dependencies.sort(key = dependency_to_key)
-
-# Check if any of the explicit dependencies is redundant
-
-redundant_dependencies: list[Dependency] = list()
-
-for dependency in dependencies:
-    if not dependency.explicit: continue
-
-    u = resources_to_node[dependency.dependee]
-    v = resources_to_node[dependency.depended]
-
-    if graph.path(u, v):
-        redundant_dependencies.append(dependency)
-    
-    graph.edge(u, v)
+redundant_dependencies = analyzer.redundant()
 
 if len(redundant_dependencies) == 0:
     print('no redundant dependencies found')
-
 for dependency in redundant_dependencies:
     print(f'redundant dependency found: {dependency}')
