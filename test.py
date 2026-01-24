496576262c4649633f0cec4b7bd0eae3d0c02b76
bogosorter <luiswbarbosa@gmail.com>
2026-01-24 22:23:42 +0000

    prevented repeated directory parsing in test.py

--- test.py
+++ test.py
@@ -1,15 +1,10 @@
 from pathlib import Path
 from parser import Parser
 from analyzer import DependencyAnalyzer
+from typing import TextIO
 
 ROOT = Path('terrads-light')
 OUTPUT = Path('output.txt')
-
-def print_progress(parsed, analyzed, redundant, failed):
-    print(f'\rParsed: {parsed}\tAnalyzed: {analyzed}\tFailed: {failed}\tRedundant dependencies: {redundant}', end='')
-
-def has_tf_files(directory: Path) -> bool:
-    return any(p.suffix == '.tf' for p in directory.iterdir() if p.is_file())
 
 parsed = 0
 analyzed = 0
@@ -17,36 +12,50 @@ redundant_count = 0
 failed = 0
 parser = Parser()
 
-with OUTPUT.open('w') as out:
-    for path in ROOT.rglob('*'):
-        if not path.is_dir() or not has_tf_files(path):
-            continue
+def process_directory(directory: Path, out: TextIO):
+    global parsed, analyzed, redundant_count, failed
 
-        try:
-            resources, dependencies = parser.parse(str(path))
-            parsed += 1
-        except Exception as e:
-            out.write(f'Error parsing {path}.\n')
-            failed += 1
-            print_progress(parsed, analyzed, redundant_count, failed)
-            continue
-
-        try:
-            analyzer = DependencyAnalyzer(resources, dependencies)
-            redundant = analyzer.redundant()
-            analyzed += 1
-            redundant_count += len(redundant)
-
-            if redundant:
-                out.write(f'Found {len(redundant)} redundant dependencies in {path}:\n')
-                for dependency, path in redundant:
-                    out.write(f'\t{dependency} via' + '\n')
-                    out.write('\t\t' + ' ->\n\t\t'.join(str(node) for node in path) + '\n')
-
-        except Exception as e:
-            out.write(f'Error analyzing {path}.\n')
-            failed += 1
-
+    if not directory.is_dir(): return
+    if not has_tf_files(directory):
+        # This is not a Terraform module directory, search deeper
+        for subdirectory in directory.iterdir():
+            process_directory(subdirectory, out)
+        return
+    
+    try:
+        resources, dependencies = parser.parse(str(directory))
+        parsed += 1
+    except Exception as e:
+        out.write(f'Error parsing {directory}.\n')
+        failed += 1
         print_progress(parsed, analyzed, redundant_count, failed)
+        return
+
+    try:
+        analyzer = DependencyAnalyzer(resources, dependencies)
+        redundant = analyzer.redundant()
+        analyzed += 1
+        redundant_count += len(redundant)
+
+        if redundant:
+            out.write(f'Found {len(redundant)} redundant dependencies in {directory}:\n')
+            for dependency, path in redundant:
+                out.write(f'\t{dependency} via' + '\n')
+                out.write('\t\t' + ' ->\n\t\t'.join(str(node) for node in path) + '\n')
+
+    except Exception as e:
+        out.write(f'Error analyzing {directory}.\n')
+        failed += 1
+
+    print_progress(parsed, analyzed, redundant_count, failed)
+
+def print_progress(parsed, analyzed, redundant, failed):
+    print(f'\rParsed: {parsed}\tAnalyzed: {analyzed}\tFailed: {failed}\tRedundant dependencies: {redundant}', end='')
+
+def has_tf_files(directory: Path) -> bool:
+    return any(p.suffix == '.tf' for p in directory.iterdir() if p.is_file())
+
+with OUTPUT.open('w') as out:
+    process_directory(ROOT, out)
 
 print('\nDetailed feedback available in output.txt')
