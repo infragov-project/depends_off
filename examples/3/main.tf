675875084160248dabc8a9ccc049c07ade92ef34
bogosorter <luiswbarbosa@gmail.com>
2026-01-27 10:26:03 +0000

    added graph provider to example 3

--- examples/3/main.tf
+++ examples/3/main.tf
@@ -4,69 +4,71 @@
  * the tool's graph export to visualize the dependencies.
  */
 
+provider "graph" {}
+
 // Case A
 
-resource "node" "a" {
-    references = [node.b]
-    depends_on = [node.c]
+resource "graph_node" "a" {
+    references = [graph_node.b]
+    depends_on = [graph_node.c]
 }
 
-resource "node" "b" {
-    references = [node.c]
+resource "graph_node" "b" {
+    references = [graph_node.c]
 }
 
-resource "node" "c" {}
+resource "graph_node" "c" {}
 
 // Case B
 
-resource "node" "d" {
-    references = [node.e]
-    depends_on = [node.f]
+resource "graph_node" "d" {
+    references = [graph_node.e]
+    depends_on = [graph_node.f]
 }
 
-resource "node" "e" {
-    depends_on = [node.f]
+resource "graph_node" "e" {
+    depends_on = [graph_node.f]
 }
 
-resource "node" "f" {}
+resource "graph_node" "f" {}
 
 // Case C
 
-resource "node" "g" {
-    depends_on = [node.h, node.i]
+resource "graph_node" "g" {
+    depends_on = [graph_node.h, graph_node.i]
 }
 
-resource "node" "h" {
-    references = [node.i]
+resource "graph_node" "h" {
+    references = [graph_node.i]
 }
 
-resource "node" "i" {}
+resource "graph_node" "i" {}
 
 // Case D
 
-resource "node" "j" {
-    references = [node.k]
-    depends_on = [node.m]
+resource "graph_node" "j" {
+    references = [graph_node.k]
+    depends_on = [graph_node.m]
 }
 
-resource "node" "k" {
-    depends_on = [node.l]
+resource "graph_node" "k" {
+    depends_on = [graph_node.l]
 }
 
-resource "node" "l" {
-    references = [node.m]
+resource "graph_node" "l" {
+    references = [graph_node.m]
 }
 
-resource "node" "m" {}
+resource "graph_node" "m" {}
 
 // Case e
 
 
-resource "node" "n" {
-    depends_on = [node.o, node.p]
+resource "graph_node" "n" {
+    depends_on = [graph_node.o, graph_node.p]
 }
 
-resource "node" "o" {
-    depends_on = [node.p]
+resource "graph_node" "o" {
+    depends_on = [graph_node.p]
 }
-resource "node" "p" {}
+resource "graph_node" "p" {}
